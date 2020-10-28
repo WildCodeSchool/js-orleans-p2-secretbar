8a798f72b9ad6e419ebf647daf746ad5ca6d265b
theo.atlani@gmail.com <theo.atlani@gmail.com>
2020-10-28 10:22:12 +0100

    fix last bugs, like prop-types

---
+++ src/components/services/ServiceList.jsx
@@ -0,0 +1,50 @@
+import Service from './Service';
+import './ServiceList.css';
+
+const services = [
+  {
+    image: 'https://zupimages.net/up/20/44/cltb.jpg',
+    service: 'Lounge Bar',
+    description:
+      "Découvrez notre nouveau lounge crée exclusivement par le meilleur designer au monde. Détendez-vous sur nos canapés Rochebobois et profitez d'une ambiance feutrée et de sa musique d'ambiance, idéal pour dégustez nos alcools d'exceptions.",
+  },
+  {
+    image: 'https://zupimages.net/up/20/44/54v9.jpg',
+    service: 'Conciergerie',
+    description:
+      "Nos employés d'exceptions sont impatients de vous rencontrer et de répondre à vos attentes et vos besoins. Leur disponibilité sans égale vous attend à l'accueil du Wild Code Bar.",
+  },
+  {
+    image: 'https://zupimages.net/up/20/44/3n9i.jpg',
+    service: 'Voituriers',
+    description:
+      "Nos chauffreurs, triés sur le volet, vous garantissent une conduite douce et agréable afin de vous ramener chez vous dans les meilleures conditions. L'expérience Wild Code Bar à vivre jusqu'à votre domicile...",
+  },
+];
+
+function ServiceList() {
+  return (
+    <div className="ServicesSection">
+      <div className="HeadTitle">
+        <h2 className="ServicesTitle">Nos services</h2>
+        <img
+          className="Goldline"
+          src="https://zupimages.net/up/20/44/hrq1.png"
+          alt="line"
+        />
+      </div>
+
+      <div className="Services">
+        {services.map((service) => (
+          <Service
+            image={service.image}
+            service={service.service}
+            description={service.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ServiceList;
