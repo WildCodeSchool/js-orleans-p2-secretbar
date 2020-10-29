90ca82d3bbe47aa6c67ddc15777b78d58df95c10
jordyngoma <ngoma.jordy@yahoo.com>
2020-10-29 12:06:31 +0100

    Fautes d'orthographe corrigés , route ouverte (mais sans destinations...)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,19 @@ import './Navbar.css';
 import logo from './image/logobar.png';
 import logoCarte from './image/logoCarte.png'
 import logoPhotos from './image/portefolio.png'
-import logoHome from './image/acceuil.png'
+import logoHome from './image/accueil.png'
+import { Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+/*import * from './components/*';
+import * from './components/*';
+import * from './components/*';*/
+
 
 
 class Navbar extends React.Component {
+
     render(){
+
         return (
             <nav className='Navbar'>
                 <div className='contain'>
@@ -16,24 +24,28 @@ class Navbar extends React.Component {
                     <img className='navbar--logo' src={logo} alt="logo" />
                 </div>
                 <div className='menu--logo'>
-                    <img className= 'logoAcceuil' src={logoHome} alt="logo" />
+                    <img className= 'logoAccueil' src={logoHome} alt="logo" />
                     <img className= 'logoCart' src={logoCarte} alt="logo" />
                     <img className= 'logoPhoto' src={logoPhotos} alt="logo" /> 
                 </div>
-                <ul className='Menu' id="menu">
+                <div className='Menu' id="menu">
                     
                     <a className='close' >X</a>
-                    
-                    <a className='' href="">
-                        Acceuil
-                    </a>
-                    <a className='' href='' >
-                        Menu de la carte
-                    </a>
-                    <a className='' href=''>
-                        livre d'or
-                    </a>
-                </ul>
+                    <div className='Onglets'>
+                    <Link to="/"> Accueil </Link>
+                    <Link to="/" > Carte </Link>
+                    <Link to="/">Livre d'or</Link>
+                    </div>
+                    <div>
+                    <Switch>
+                    <Route exact path="/" component={}/>
+                    <Route path="/" component={}/>
+                    <Route path="/" component={}/>
+                    <Route path="/" component={}/>
+                    </Switch>
+                    </div>
+                    </div>
+                
                 <a href="#menu"className='burger'>
                     <div></div>
                     <div></div>
